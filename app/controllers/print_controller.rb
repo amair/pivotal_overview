21b5f8ff943adf23a8ddec9652ca890e79c820c0
Alastair Mair <alastair@ubuntu.(none)>
2013-01-14 17:19:26 +0000

    Store token in a cookie and allow updating

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -3,6 +3,20 @@ class PrintController < ApplicationController
   def index
     @state_selection = ["unscheduled", "unstarted", "started", "finished", "delivered", "accepted", "rejected"]
 
+
+    token = params[:token]
+
+    unless token.nil?
+      Token.update_token(token)
+      cookies.permanent[:token] = token
+      logger.debug "Retrieve Pivotal token #{token} from page submission and store in cookie"
+    end
+
+    unless cookies[:token].nil?
+      Token.update_token(cookies[:token])
+      logger.debug ("Using token #{cookies[:token]} from the session cookie")
+    end
+
     @projects = ProjectData.find_all_projects
 
   end
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,20 +1,5 @@
 class Token
   def self.get_token
-    #logger.debug "#{params}"
-    if @token.nil?
-      if params[:token]
-        @token = params[:token]
-        cookies.permanent[:token] = @token unless @token.nil?
-        #logger.debug "Retrieve Pivotal token #{@token} from page submission and store in cookie"
-      else
-        if cookies[:token].nil?
-          logger.fatal "Cannot figure out tracker token - giving up"
-        else
-          @token = cookies[:token]
-          #logger.debug "Retrieving token #{@token} from cookie"
-        end
-      end
-    end
     @token
   end
 
