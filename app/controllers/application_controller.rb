79219c0510de6d8addff4687e13dbfd58625beb7
Alastair Mair <alastair@ubuntu.(none)>
2013-01-17 15:09:12 +0000

    Add some styling to the page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :get_filtered_stories
+  helper_method :get_filtered_stories, :get_current_year
 
   def get_filtered_stories(project)
     filter = params[:state]  || ["unscheduled", "unstarted"]
     ProjectData.find_all_stories(project, filter )
+  end
+
+  def get_current_year
+    date=Date.today
+    date.year
   end
 
 end
