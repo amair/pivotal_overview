349dc6583b28065a24d6dae6299d35a411963b14
Alastair Mair <alastair@ubuntu.(none)>
2013-01-21 10:40:24 +0000

    Should be a class instance variable in Token

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,17 +1,13 @@
 class Token
+  @token=nil   #N.B. Class Instance Variable
 
   def self.get_token
     PivotalTracker::Client.token = @token
-    token_defined?
+    @token.present?
   end
 
   def self.update_token( new_token)
     @token = new_token
   end
 
-  private
-
-  def self.token_defined?
-    @token.nil? == false
-  end
 end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe 'Token'  do
+  it 'should start without a token defined' do
+    Token.get_token.should eq(false)
+  end
+
   it 'should retrieve the stored token' do
     myToken="ABC123"
     Token.update_token(myToken)
-    Token.get_token.should eq myToken
+    Token.get_token.should eq(true)
   end
 
 end
